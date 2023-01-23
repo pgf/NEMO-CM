e1da32ae38e56d9b2e6e1a21439ab6ba4559049a
Daniele Peano <daniele.peano@cmcc.it>
2023-01-23 16:05:31 +0100

    Update interface folder structure

---
+++ interface/ext/ioipsl.F90
@@ -0,0 +1,21 @@
+#if defined CCSMCOUPLED
+#define calendar calendar_ioipsl
+#endif
+
+MODULE ioipsl
+!
+!$Id$
+!-
+! This software is governed by the CeCILL license
+! See IOIPSL/IOIPSL_License_CeCILL.txt
+!
+  USE errioipsl 
+  USE stringop
+  USE mathelp    
+  USE getincom
+  USE calendar   
+  USE fliocom    
+  USE flincom    
+  USE histcom    
+  USE restcom
+END MODULE ioipsl
